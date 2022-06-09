e2e8bde5f6d45a707bf9aa77e2bf0b1eb5e94ca3
PropolisWesly <32191591+PropolisWesly@users.noreply.github.com>
2022-06-09 15:42:24 +0300

    Add files via upload

---
+++ Объектно Ориентрованное Програмирование/Цепочка_обязанностей/Solution.py
@@ -0,0 +1,102 @@
+from abc import ABC, abstractmethod
+
+
+class SomeObject:
+    def __init__(self):
+        self.integer_field = 0
+        self.float_field = 0.0
+        self.string_field = ""
+
+
+class NullHandler:
+    def __init__(self, successor=None):
+        self.successor = successor
+
+    def handle(self, obj, event):
+        if self.successor is not None:
+            self.successor.handle(obj, event)
+
+
+class IntHandler(NullHandler):
+    def handle(self, obj, event):
+        if event.kind == 'event_set':
+            self.__set_handler(obj, event)
+        elif event.kind == 'event_get':
+            return self.__get_handler(obj, event)
+        else:
+            raise ValueError(f'This kind of event doesn\'t exist {event.kind}')
+
+    def __set_handler(self, obj, event):
+        if isinstance(event.value, int):
+            obj.integer_field = event.value
+        else:
+            self.successor.handle(obj, event)
+
+    def __get_handler(self, obj, event):
+        if issubclass(event.typer, int):
+            return obj.integer_field
+        else:
+            return self.successor.handle(obj, event)
+
+
+class FloatHandler(NullHandler):
+    def handle(self, obj, event):
+        if event.kind == 'event_set':
+            self.__set_handler(obj, event)
+        elif event.kind == 'event_get':
+            return self.__get_handler(obj, event)
+        else:
+            raise ValueError(f'This kind of event doesn\'t exist {event.kind}')
+
+    def __set_handler(self, obj, event):
+        if isinstance(event.value, float):
+            obj.float_field = event.value
+        else:
+            self.successor.handle(obj, event)
+
+    def __get_handler(self, obj, event):
+        if issubclass(event.typer, float):
+            return obj.float_field
+        else:
+            return self.successor.handle(obj, event)
+
+
+class StrHandler(NullHandler):
+    def handle(self, obj, event):
+        if event.kind == 'event_set':
+            self.__set_handler(obj, event)
+        elif event.kind == 'event_get':
+            return self.__get_handler(obj, event)
+        else:
+            raise ValueError(f'This kind of event doesn\'t exist {event.kind}')
+
+    def __set_handler(self, obj, event):
+        if isinstance(event.value, str):
+            obj.string_field = event.value
+        else:
+            self.successor.handle(obj, event)
+
+    def __get_handler(self, obj, event):
+        if issubclass(event.typer, str):
+            return obj.string_field
+        else:
+            return self.successor.handle(obj, event)
+
+
+class EventGet:
+    def __init__(self, typer):
+        self.typer = typer
+        self.kind = 'event_get'
+
+
+class EventSet:
+    def __init__(self, value):
+        self.value = value
+        self.kind = 'event_set'
+
+
+# a = IntHandler(NullHandler())
+# print(a.successor.successor)
+# b = NullHandler()
+# obj = SomeObject()
+# b.handle(obj, EventGet(32))
